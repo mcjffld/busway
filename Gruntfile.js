7eaa2d7f7cd6536dcb691302bcc3990442242ec3
Michael James <Michael.James@priceline.com>
2016-09-05 09:25:58 -0400

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -278,29 +278,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // The following *-min tasks produce minified files in the dist folder
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.{png,jpg,jpeg,gif}',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= yeoman.client %>/assets/images',
-          src: '{,*/}*.svg',
-          dest: '<%= yeoman.dist %>/public/assets/images'
-        }]
-      }
-    },
-
     // Allow the use of non-minsafe AngularJS files. Automatically makes it
     // minsafe compatible so Uglify does not destroy the ng references
     ngAnnotate: {
@@ -430,9 +407,7 @@ module.exports = function (grunt) {
       },
       dist: [
         'babel',
-        'sass',
-        'imagemin',
-        'svgmin'
+        'sass'
       ]
     },
 
